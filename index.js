1c3deece2149587dcb5cd5e227692498c6fce5fb
Gijsjan <agijsbro@gmail.com>
2017-09-21 16:23:15 +0200

    Bump deps

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const hire_forms_autocomplete_1 = require("hire-forms-autocomplete");
 const autocomplete = (props) => React.createElement(hire_forms_autocomplete_1.default, Object.assign({}, props, { onChange: (value) => props.onChange(props.values.concat(value)), onInputChange: props.onInputChange }));
 const list = (props) => React.createElement(hire_forms_list_1.default, Object.assign({}, props, { mutable: true, onChange: (values) => props.onChange(values) }));
 const AutocompleteList = (props) => React.createElement("div", { className: "hire-forms-autocomplete-list" },
-    props.renderListBeforeAutocomplete ? list(props) : autocomplete(props),
-    props.renderListBeforeAutocomplete ? autocomplete(props) : list(props));
+    props.listBeforeAutocomplete ? list(props) : autocomplete(props),
+    props.listBeforeAutocomplete ? autocomplete(props) : list(props));
 exports.default = AutocompleteList;
--- index.tsx
+++ index.tsx
@@ -1,31 +1,33 @@
 // TODO Fix filter options (remove values from options).
 // Doesn't work, because AutoComplete has a cache for the async.
 import * as React from 'react';
-import List from 'hire-forms-list';
-import Autocomplete from 'hire-forms-autocomplete';
+import List, {IListProps} from 'hire-forms-list';
+import Autocomplete, {IAutocompleteProps} from 'hire-forms-autocomplete';
 
 interface IAutocompleteListProps {
-	renderListBeforeAutocomplete: boolean;
+	listBeforeAutocomplete?: boolean;
 }
 
-const autocomplete = (props) =>
+type ICombined = IAutocompleteListProps & IAutocompleteProps & IListProps;
+
+const autocomplete = (props: ICombined) =>
 	<Autocomplete
 		{...props}
 		onChange={(value) => props.onChange(props.values.concat(value))}
 		onInputChange={props.onInputChange}
 	/>;
 
-const list = (props) =>
+const list = (props: ICombined) =>
 	<List
 		{...props}
 		mutable
 		onChange={(values) => props.onChange(values)}
 	/>;
 
-const AutocompleteList = (props: IAutocompleteListProps) =>
+const AutocompleteList: React.SFC<ICombined> = (props) =>
 	<div className="hire-forms-autocomplete-list">
-		{props.renderListBeforeAutocomplete ? list(props) : autocomplete(props)}
-		{props.renderListBeforeAutocomplete ? autocomplete(props) : list(props)}
+		{props.listBeforeAutocomplete ? list(props) : autocomplete(props)}
+		{props.listBeforeAutocomplete ? autocomplete(props) : list(props)}
 	</div>;
 
 export default AutocompleteList;
